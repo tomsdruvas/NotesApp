538ecdbb06217d39434927cda568f703cda5ac6f
Toms Druvaskalns <tomsdruvas@gmail.com>
2022-03-08 21:28:00 +0000

    created react app

---
+++ client/src/containers/MainContainer.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import '../styles/Style.css'
+
+const MainContainer = () => {
+  return (
+    <div class="parent">
+    <div class="div1">
+    <form>
+  <input type="text" name="search" placeholder="Search.." />
+    </form>
+    
+    
+    
+    
+    
+    
+     </div>
+    <div class="div2">Hello </div>
+    </div>
+  )
+}
+
+export default MainContainer
