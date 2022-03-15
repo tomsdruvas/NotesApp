2c04fbb1b728e035542c241211601ca7eeffc258
Toms Druvaskalns <tomsdruvas@gmail.com>
2022-03-15 22:34:48 +0000

    made image model

--- server/src/main/java/WoodMac/NotesApp/NotesAppApplication.java
+++ server/src/main/java/WoodMac/NotesApp/NotesAppApplication.java
@@ -1,11 +1,21 @@
 package WoodMac.NotesApp;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@ComponentScan({"com.delivery.request"})
+@EntityScan("com.delivery.domain")
+//@EnableJpaRepositories("com.delivery.repository")
 public class NotesAppApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(NotesAppApplication.class, args);
 	}
