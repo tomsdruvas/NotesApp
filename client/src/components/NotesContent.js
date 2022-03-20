19c360f692181bb0982ef6e20d4cb0e9cb22ea39
Toms Druvaskalns <tomsdruvas@gmail.com>
2022-03-20 17:54:12 +0000

    error fix when there are 0 items on the notes list from API

--- client/src/components/NotesContent.js
+++ client/src/components/NotesContent.js
@@ -5,14 +5,14 @@ import MDEditor from '@uiw/react-md-editor';
 const NotesContent = ({selectedNotesItem}) => {
   return (
       <>
-    <p className="contentTitle">{selectedNotesItem.title ?? "Welcome!"}</p>
-   <p className="contentDates"> 
-   Posted: {selectedNotesItem.postDate ?? moment().format('MMM Do YYYY, h:mm')}
-   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-    Updated: {selectedNotesItem.updateDate ?? moment().format('MMM Do YYYY, h:mm')}</p>
+      <p className="contentTitle">{selectedNotesItem?.title ?? "Welcome!"}</p>
+      <p className="contentDates"> 
+      Posted: {selectedNotesItem?.postDate ?? moment().format('MMM Do YYYY, h:mm')}
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      Updated: {selectedNotesItem?.updateDate ?? moment().format('MMM Do YYYY, h:mm')}</p>
 
     <div className="divForContent">
-    <MDEditor.Markdown source={selectedNotesItem.content ?? "Welcome to my new notes taking app"} />
+    <MDEditor.Markdown source={selectedNotesItem?.content ?? "Welcome to my new notes taking app"} />
   
 
 
